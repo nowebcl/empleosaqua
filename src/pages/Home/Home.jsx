94637197130346a771a7e5b0f9c3cd1989a2c909
nowebcl <nowebcl@gmail.com>
2026-03-26 15:00:18 -0300

    Refactor sidebar ads layout to properly flank content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -18,55 +18,62 @@ const Home = () => {
   return (
     <div className="app-container">
       <Header />
-      <main className="relative">
-        {/* Publicidad Lateral Izquierda - Fija a los costados exteriores */}
-        <div className="hidden min-[1600px]:block fixed left-4 top-32 w-[160px] 2xl:w-[200px] z-40">
-          <AdBanner 
-            src="https://tpc.googlesyndication.com/simgad/4141843103343703535" 
-            href="https://www.aqua-forum.cl/" 
-            alt="AquaForum Los Lagos 29 Abril" 
-          />
-        </div>
-
-        {/* Publicidad Lateral Derecha - Fija a los costados exteriores */}
-        <div className="hidden min-[1600px]:block fixed right-4 top-32 w-[160px] 2xl:w-[200px] z-40">
-          <AdBanner 
-            src="https://tpc.googlesyndication.com/simgad/18162141951724842782" 
-            href="https://www.aqua-forum.cl/" 
-            alt="AquaForum Internacional 05 Nov" 
-          />
-        </div>
-
+      <main className="w-full relative overflow-x-hidden">
         <Hero />
         
-        {/* Publicidad: Banner horizontal */}
-        <div className="container mx-auto px-4 mt-8 mb-4">
-          <AdBanner 
-            src="https://tpc.googlesyndication.com/simgad/8863766421333283423" 
-            href="https://www.linkedin.com/company/ddconsultingchile/posts/?feedView=all" 
-            alt="DD Reclutamiento y Selección" 
-          />
-        </div>
-
-        <QuickActions />
-
-        <div className="container mx-auto px-4 mb-16 flex flex-col lg:flex-row gap-8">
-          <div className="flex-1" style={{ minWidth: 0 }}>
-            {/* The FeaturedJobs will now occupy this left container */}
-            <FeaturedJobs />
+        {/* Layout Principal centrado con Sidebar Ads a los costados exteriores */}
+        <div className="w-full max-w-[1550px] mx-auto flex items-start justify-center relative px-2">
+          
+          {/* Publicidad Lateral Izquierda (Sticky) */}
+          <div className="hidden xl:block sticky top-24 w-[160px] 2xl:w-[200px] shrink-0 mr-4 mt-8">
+            <AdBanner 
+              src="https://tpc.googlesyndication.com/simgad/4141843103343703535" 
+              href="https://www.aqua-forum.cl/" 
+              alt="AquaForum Los Lagos 29 Abril" 
+            />
           </div>
 
-          <aside className="lg:w-[300px] w-full shrink-0 flex flex-col gap-6 pt-16">
-            <AdBanner 
-              src="https://tpc.googlesyndication.com/simgad/16919953663833581379" 
-              href="https://www.facebook.com/Empleosaqua" 
-              alt="Empleos Aqua Redes Sociales" 
-            />
-          </aside>
-        </div>
+          {/* Contenido Central */}
+          <div className="flex-1 w-full max-w-[1200px]">
+            {/* Publicidad: Banner horizontal */}
+            <div className="w-full px-2 lg:px-4 mt-8 mb-4">
+              <AdBanner 
+                src="https://tpc.googlesyndication.com/simgad/8863766421333283423" 
+                href="https://www.linkedin.com/company/ddconsultingchile/posts/?feedView=all" 
+                alt="DD Reclutamiento y Selección" 
+              />
+            </div>
 
-        <Institutional />
-        <Stats />
+            <QuickActions />
+
+            <div className="w-full px-2 lg:px-4 mb-16 flex flex-col lg:flex-row gap-8">
+              <div className="flex-1" style={{ minWidth: 0 }}>
+                {/* The FeaturedJobs will now occupy this left container */}
+                <FeaturedJobs />
+              </div>
+
+              <aside className="lg:w-[300px] w-full shrink-0 flex flex-col gap-6 pt-16">
+                <AdBanner 
+                  src="https://tpc.googlesyndication.com/simgad/16919953663833581379" 
+                  href="https://www.facebook.com/Empleosaqua" 
+                  alt="Empleos Aqua Redes Sociales" 
+                />
+              </aside>
+            </div>
+
+            <Institutional />
+            <Stats />
+          </div>
+
+          {/* Publicidad Lateral Derecha (Sticky) */}
+          <div className="hidden xl:block sticky top-24 w-[160px] 2xl:w-[200px] shrink-0 ml-4 mt-8">
+            <AdBanner 
+              src="https://tpc.googlesyndication.com/simgad/18162141951724842782" 
+              href="https://www.aqua-forum.cl/" 
+              alt="AquaForum Internacional 05 Nov" 
+            />
+          </div>
+        </div>
       </main>
       <Footer />
     </div>
